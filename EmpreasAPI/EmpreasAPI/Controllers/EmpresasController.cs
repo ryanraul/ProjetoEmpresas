972267281d66457d79b19f07fd886cdf662778d7
Raul Ryan <ryanraul_11@hotmail.com>
2020-08-23 14:40:51 -0300

    Definindo como sera a consulta a API da receita federal para cadastro das empresas

--- EmpreasAPI/EmpreasAPI/Controllers/EmpresasController.cs
+++ EmpreasAPI/EmpreasAPI/Controllers/EmpresasController.cs
@@ -5,6 +5,8 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace EmpreasAPI.Controllers
@@ -31,11 +33,39 @@ namespace EmpreasAPI.Controllers
             [FromServices] DataContext context,
             [FromBody] Empresa model)
         {
+            var verif_cnpj = await context.Empresas.FirstOrDefaultAsync(x => x.Cnpj == model.Cnpj);
+            if (verif_cnpj != null)
+            {
+                return BadRequest(new { message = "Cnpj ja Cadastrado" });
+            }
+
             if (ModelState.IsValid)
             {
-                context.Empresas.Add(model);
-                //await context.SaveChangesAsync();
-                return model;
+                var httpClient = HttpClientFactory.Create();
+                var url = $"https://www.receitaws.com.br/v1/cnpj/{model.Cnpj}";
+                HttpResponseMessage httpResponseMessage = await httpClient.GetAsync(url);
+
+                if (httpResponseMessage.StatusCode == HttpStatusCode.OK)
+                {
+                    var content = httpResponseMessage.Content;
+                    var data = await content.ReadAsAsync<Empresa>();
+                    if(data.status != "OK")
+                    {
+                        return NotFound(new { message = $"{data.Message}" });
+                    }
+                    else
+                    {
+                        data.Cnpj = model.Cnpj;
+                        context.Empresas.Add(data);
+
+                        //await context.SaveChangesAsync();
+                        return data;
+                    }
+                }
+                else
+                {
+                    return BadRequest(ModelState);
+                }
             }
             else
             {
