01c29eda95740d1242ea4e50bfb9b8abc3c562a8
ryanraul <ryanraul_11@hotmail.com>
2020-09-22 08:25:29 -0300

    Criando metodo para realizacao de requisicao na ReceitaWS via CNPJ

--- EmpreasAPI/EmpreasAPI/Controllers/EmpresasController.cs
+++ EmpreasAPI/EmpreasAPI/Controllers/EmpresasController.cs
@@ -34,6 +34,28 @@ namespace EmpreasAPI.Controllers
             return empresas;
         }
 
+        public async Task<ActionResult<Empresa>> RequisicaoWebService(string cnpj)
+        {
+            var httpClient = HttpClientFactory.Create();
+            var url = $"https://www.receitaws.com.br/v1/cnpj/{cnpj}";
+            HttpResponseMessage httpResponseMessage = await httpClient.GetAsync(url);
+
+            if (httpResponseMessage.StatusCode == HttpStatusCode.OK)
+            {
+                var content = httpResponseMessage.Content;
+                var data = await content.ReadAsAsync<Empresa>();
+                if (data.Status != "OK")
+                {
+                    return Ok(new { message = $"{data.Message}" });
+                }
+                return data;
+            }
+            else
+            {
+                return Ok(new { message = $"Aguarde um pouco ate o proximo registro (Erro: {httpResponseMessage.StatusCode})" });
+            }
+        }
+
         [HttpGet]
         [Route("")]
         public async Task<ActionResult<List<Empresa>>> Get([FromServices] DataContext context)
@@ -67,6 +89,7 @@ namespace EmpreasAPI.Controllers
             [FromServices] DataContext context,
             int id)
         {
+            
             var empresa = await context.Empresas
                 .Include(x => x.AtividadePrincipal)
                 .Include(x => x.AtividadesSecundarias)
@@ -85,37 +108,20 @@ namespace EmpreasAPI.Controllers
         {
             var verificarCnpj = await context.Empresas.FirstOrDefaultAsync(x => x.Cnpj == model.Cnpj);
             if (verificarCnpj != null)
-            {
                 return Ok(new { message = "Cnpj ja Cadastrado" });
-            }
+
 
             if (ModelState.IsValid)
             {
-                var httpClient = HttpClientFactory.Create();
-                var url = $"https://www.receitaws.com.br/v1/cnpj/{model.Cnpj}";
-                HttpResponseMessage httpResponseMessage = await httpClient.GetAsync(url);
 
-                if (httpResponseMessage.StatusCode == HttpStatusCode.OK)
+                var data = await RequisicaoWebService(model.Cnpj);
+                if (data.Result == null)
                 {
-                    var content = httpResponseMessage.Content;
-                    var data = await content.ReadAsAsync<Empresa>();
-                    if(data.Status != "OK")
-                    {
-                        return Ok(new { message = $"{data.Message}" });
-                    }
-                    else
-                    {
-                        data.Cnpj = model.Cnpj;
-                        context.Empresas.Add(data);
-
-                        await context.SaveChangesAsync();
-                        return data;
-                    }
+                    data.Value.Cnpj = model.Cnpj;
+                    context.Empresas.Add(data.Value);
+                    await context.SaveChangesAsync();
                 }
-                else
-                {
-                    return Ok(new { message = $"Aguarde um pouco ate o proximo registro (Erro: {httpResponseMessage.StatusCode})"});
-                }
+                return data;
             }
             else
             {
